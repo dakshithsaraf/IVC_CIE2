5be25def4dbc1841cf4ee2745cdfe2e9334d5988
Dakshih <dakshihsaraf@gmailcom>
2026-03-26 13:03:41 +0530

    first commit

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+def is_palindrome(num):
+    
+    str_num = str(num)
+    return str_num == str_num[::-1]
+
+def sum_of_digits(num):
+    
+    return sum(int(digit) for digit in str(abs(num)))
+
+n = int(input("Enter an integer N: "))
+
+digit_sum = sum_of_digits(n)
+
+if is_palindrome(digit_sum):
+    print("palindrome")
+else:
+    print("not palindrome")
